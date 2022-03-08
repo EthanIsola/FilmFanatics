84b463c40409292e951ed6aa27a37423f80c843c
Ethan Isola <isolaethan@gmail.com>
2022-03-08 08:25:38 -0600

    added functioning fetch statement

--- film_fanatics/src/components/MovieList.js
+++ film_fanatics/src/components/MovieList.js
@@ -1,7 +1,11 @@
 import Movie from './Movie'
+import {useEffect, useState} from 'react'
 
 function MovieList(movies) {
-
+    useEffect(()=>{
+        fetch("http://localhost:9292/movies")
+        .then(r=>r.json())
+        .then(data=> console.log(data))},[])
     return <h1>movie list</h1>
 }
 
