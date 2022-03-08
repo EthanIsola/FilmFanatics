17353c4458c1a1d8622cfb83f95c65612f952da1
Ethan Isola <isolaethan@gmail.com>
2022-03-08 09:02:31 -0600

    renders movie data

--- film_fanatics/src/components/Movie.js
+++ film_fanatics/src/components/Movie.js
@@ -1,6 +1,10 @@
 
-function Movie() {
-
+function Movie({title, release_date, cast, director, producer, image_URL}) {
+    return(
+        <div>
+            <h1>{title}</h1>
+        </div>
+    ) 
 }
 
 export default Movie;
--- film_fanatics/src/components/MovieList.js
+++ film_fanatics/src/components/MovieList.js
@@ -1,12 +1,33 @@
 import Movie from './Movie'
 import {useEffect, useState} from 'react'
 
-function MovieList(movies) {
+function MovieList() {
+
+    const [movies, setMovies] = useState([])
+    const [displayData, setDisplay] = useState(false)
+
     useEffect(()=>{
         fetch("http://localhost:9292/movies")
         .then(r=>r.json())
-        .then(data=> console.log(data))},[])
-    return <h1>movie list</h1>
+        .then(data=> setMovies(data))
+    },[])
+    useEffect(()=>{
+        console.log(movies)
+        setDisplay(
+            movies.map((movie)=>{
+                return <Movie title = {movie.title} release_date = {movie.release_date} cast = {movie.cast} director = {movie.director}  producer = {movie.producer} image_URL = {movie.image_URL} key = {movie.id}/> 
+                                }
+                        )
+                    )
+    }, [movies])
+
+    
+
+    return (
+        <div>
+            {displayData}
+        </div>
+    )
 }
 
 export default MovieList;
