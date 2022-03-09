3c76fdd953660636772797822fed7efbe2e44c53
yeohoon yoon <yhoon.yoon@gmail.com>
2022-03-09 16:04:29 -0500

    movie page

--- film_fanatics/src/components/MoviePage.js
+++ film_fanatics/src/components/MoviePage.js
@@ -1,11 +1,114 @@
-import { useEffect, useState } from "react";
-function MoviePage({movie}) {
+import {useEffect, useState} from 'react'
+function MoviePage() {
+
+    const [reviews, setReviews] = useState([]);
+    const [updatedR, setUpdatedR] = useState(true)
+
+    const [newReviews, setNewReviews] = useState("");
+    const [name, setName] = useState("")
+
+    useEffect(() => {
+        fetch(`http://localhost:9292/reviews`)
+          .then((r) => r.json())
+          .then((movieReviews) => 
+          setReviews(movieReviews));
+      }, []);
+
+ 
+      useEffect(() => {
+        fetch(`http://localhost:9292/reviews`)
+          .then((r) => r.json())
+          .then((movieReviews) => 
+          setReviews(movieReviews));
+      }, [updatedR]);
+ 
+
+    const newReview = {
+        name:name,
+        review:newReviews
+    }
+   
+    function handleSubmit(e) {
+   
+      
+        fetch("http://localhost:9292/reviews", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newReview),
+        })
+          .then((r) => r.json())
+          .then((newReviews) => {
+              console.log(newReviews)
+              setReviews([newReviews, ...reviews])
+          })
+    }
+      
+    function handleDeleteClick(e){
+       
+        fetch(`http://localhost:9292/reviews/${e.target.value}`, {
+            method: "DELETE",
+        })
+        setUpdatedR(!updatedR)
+    }
+        const listReview = reviews.map((review)=>{
+            return (
+                <div>
+                  <div> 
+                    <label>Name:</label>
+                    <p>{review.name} </p>
+                  </div>
+                  <div> 
+                    <label>Review:</label>
+                    <p>{review.review}  </p>  
+                  </div>
+            <button value={review.id} onClick={handleDeleteClick}>delete</button>
+            {/* <button value={review.id} onClick={handleEditClick}>edit</button> */}
+            </div>
+            )
+        }) 
+
+       
     return (
         <div>
-            <h1>{movie.title}</h1>
+            
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label>Name:</label>
+                        <input type="text" name="name" value={name}
+                        onChange ={(e) => setName(e.target.value)}/>
+                    </div>
+                    <div>
+                        <label>Review:</label>
+                        <input type="text" placeholder="any comments" value={newReviews} onChange ={(e) => setNewReviews(e.target.value)}/>
+                        <input type="submit" />
+                    </div>
+                </form>
+            </div>
+            <div>
+                {listReview}
+            </div>
+
         </div>
     )
 
 }
 
 export default MoviePage;
+
+
+    // function handleEditClick(e){
+    //     e.preventDefault();
+
+    //     fetch(`http://localhost:9292/reviews/${e.target.value}`, {
+    //         method: "PATCH",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //         },
+    //         body:JSON.stringify(newReview)
+    //     })
+    //     .then((r) =>r.json())
+    //     .then((editReview) => setReviews(editReview))
+    // }
