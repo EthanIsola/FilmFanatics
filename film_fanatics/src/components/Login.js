59d9928e4677acd8fc18454395b20a09ebb19b94
Ethan Isola <isolaethan@gmail.com>
2022-03-10 19:29:31 -0600

    Fully functioning login feature

--- film_fanatics/src/components/Login.js
+++ film_fanatics/src/components/Login.js
@@ -5,6 +5,8 @@ function Login({user, setUser}) {
     const [userName, setUserName] = useState("");
     const [myPassword, setPassword] = useState("");
     const [age, setAge] = useState("");
+    const [option, setOption] = useState("login")
+    const [display, setOptionDisplay] = useState(false)
     
 
     function handleUserName(event) {
@@ -19,23 +21,31 @@ function Login({user, setUser}) {
         setAge(event.target.value);
     }
 
-
+    function handleOption(event) {
+        setOption(event.target.value);
+        if(event.target.value == "sign up"){
+            setOptionDisplay(true)
+        }
+        else{
+            setOptionDisplay(false)
+        }
+    }
 
     useEffect(()=>{
-        fetch("http://localhost:9292/viewers")
-        .then(r=>r.json())
-        .then(data=> currentUsers = data)
+        
       },[])
 
       function handleSubmit(e) {
         e.preventDefault();
-
-        const formData = {
-            name: userName,
+        if (option === "sign up") {
+            console.log("sign up")
+            const formData = {
             password: myPassword,
+            name: userName,
             age: age
         };
-    
+        alert("submitted")
+
         fetch("http://localhost:9292/viewers", {
             method: "POST",
             headers: {
@@ -43,15 +53,41 @@ function Login({user, setUser}) {
             },
             body: JSON.stringify(formData),
       })
-      setUser(userName)
-      }
+      setUser(userName)}
+      else{
+        fetch("http://localhost:9292/viewers")
+        .then(r=>r.json())
+        .then(data=>{
+        let isIncluded = false
+        for(let x in data){
+            console.log(data[x])
+            if((data[x].name === userName)&&(data[x].password === myPassword)){
+                isIncluded = true;
+            }
+        }
+        if(isIncluded === true){
+            setUser(userName)
+            alert("Logged In Successfully")
+        }
+        else{
+            alert("User Not Found")
+        }
+      })
+      }}
 
       return(
         <div>
             <form onSubmit={handleSubmit}>
+                <select
+                    className="ui selection dropdown"
+                    onChange={handleOption}
+                    value = {option}>
+                    <option value= "login">Login</option>
+                    <option value = "sign up">Sign Up</option>
+                </select>  
                 <input type="text" placeholder = "UserName" onChange={handleUserName} value={userName} />
                 <input type="text" placeholder = "Password" onChange={handlePassword} value={myPassword} />
-                <input type="text" placeholder = "Age" onChange={handleAge} value={age} />
+                {display ? <input type="text" placeholder = "Age" onChange={handleAge} value={age} /> : null}
                 <button type="submit">Submit</button>
             </form>
         </div>
